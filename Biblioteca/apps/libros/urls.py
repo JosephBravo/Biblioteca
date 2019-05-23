e864ebc98bad61fd53549282ad75f48bfc7fff0a
JosephBravo <39282996+JosephBravo@users.noreply.github.com>
2019-05-23 16:09:42 -0500

    Add files via upload

--- Biblioteca/apps/libros/urls.py
+++ Biblioteca/apps/libros/urls.py
@@ -5,5 +5,5 @@ from apps.libros.views import index, libros_view, buscar_view
 urlpatterns = [
     path('', views.index, name='index'),
     path('nuevo', libros_view, name='libro_nuevo'),
-    path('buscar', buscar_view, name='buscar')
+    path('buscar', buscar_view.as_view(), name='buscar')
       ]
--- Biblioteca/apps/libros/views.py
+++ Biblioteca/apps/libros/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from apps.libros.forms import LibrosForm
-
 from django.views.generic import TemplateView
+from .models import Libro, Autor
 	
 # Create your views here.
 
@@ -22,13 +22,31 @@ def libros_view(request):
 	return render(request, 'formulario.html', {'form':form})
 
 
-def buscar_view(request):
+class buscar_view(TemplateView):
+
+		def post(self, request, *args, **kwargs):
+			buscar = request.POST['buscalo']
+			libros = Libro.objects.filter(titulo__contains=buscar)
+			if libros:
+				datos = []
+				for libro in libros:
+					autores = libro.Autor.all()
+					datos.append(dict([(libro,autores)]))
+					return render(request, 'buscar.html',
+						{'datos':datos})
+			else: 
+				autores = Autor.objects.filter(nombre__contains=buscar)
+			return render(request, 'buscar.html',
+				{'autores':autores, 'autor':True}) 
+			
+
+"""def buscar_view(request):
 	#template_name = 'buscar.html'
 		return render(request, 'buscar.html') 
 		buscar = request.POST['buscalo']
 		
-		if post(self, request, *args, **kwargs):
+		def post(self, request, *args, **kwargs):
 			return render(request, 'buscar.html') 
-			print (buscar)
+			print (buscalo)"""
 	
 
